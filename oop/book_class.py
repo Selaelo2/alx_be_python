c0a7c2ab628e274af824a113cac6b093abaa4968
Selaelo2 <selaelolanga10@gmail.com>
2025-01-26 19:53:14 +0200

    Create book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+class Book:
+    # Constructor method (__init__)
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+        print(f"Creating Book: {self.title} by {self.author}, published in {self.year}")
+
+    # Destructor method (__del__)
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    # String representation method (__str__)
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    # Official representation method (__repr__)
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
